ef95ddf39d823ddc85944f6851ef926cb94920ed
Jose Mulet Pol <josep@iMac-de-Jose.local>
2018-03-24 19:49:16 +0100

    added index

---
+++ index.js
@@ -0,0 +1,15 @@
+/* Shorthand */
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./TsGenClass"));
+__export(require("./TsGenConstructor"));
+__export(require("./TsGenDeclaration"));
+__export(require("./TsGenConstructor"));
+__export(require("./TsGenFunc"));
+__export(require("./TsGenMethod"));
+__export(require("./TsGenImport"));
+__export(require("./TsGenParam"));
+__export(require("./TsGenSource"));
